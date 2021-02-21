6f2ba8eef97fae31aa9edd30b4b8c1c67b45e3a6
iakay <that_see@hotmail.com>
2021-02-21 23:25:18 +0300

    push notification

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { TouchableOpacity, Text, TextInput, StyleSheet, Image, AsyncStorage, Button, Platform, Keyboard,  TouchableWithoutFeedback } from 'react-native';
 
 import { View } from '../components/Themed';
@@ -12,6 +12,16 @@ import activityColor from '../hooks/activityColor';
 const color = activityColor();
 import ipAddress from '../hooks/ipAddress';
 const ip = ipAddress();
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function RegisterScreen(props: any) {
   const { updateMode, emailP, sexP, nameP, birthDateP, languageP } = props;
@@ -26,6 +36,10 @@ export default function RegisterScreen(props: any) {
   const [colorText, setColorText] = useState();
   const [isWarmVisible, setWarmVisible] = useState(false);  
   const logoutFlag = false;
+  const [pushToken, setPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
   const options = [
     { label: "MALE", value: "MALE" },
@@ -57,7 +71,7 @@ export default function RegisterScreen(props: any) {
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({ email, sex, name, birthDate, language }),
+      body: JSON.stringify({ email, sex, name, birthDate, language, pushToken }),
     };
     fetch(ip + '/api/v1/addUser', requestOptions).then((response) => response.json()).then((data) => {      
       //console.log(data);
@@ -78,6 +92,7 @@ export default function RegisterScreen(props: any) {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, sex, name, birthDate, language, id })
           };
+          // schedulePushNotification();
           fetch(ip + '/api/v1/updateUser', requestOptions).then((response) => response.json()).then((data) => {            
             setModalVisible(true);
             props.reRender();
@@ -133,6 +148,65 @@ export default function RegisterScreen(props: any) {
     setWarmVisible(false);
     props.navigation.replace('Root');
   };
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
+
+  async function schedulePushNotification() {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: 'Here is the notification body',
+        data: { data: 'goes here' },
+      },
+      trigger: { seconds: 2 },
+    });
+  }
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    return token;
+  }
 
   return (    
       <View style={styles.container}>        
